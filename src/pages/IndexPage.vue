194129818851140a5f8a0cc965879002bbd72dac
Benjamin <benjamin.guittons@outlook.com>
2022-07-04 09:38:04 +0200

    Ajout map TomTom

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="flex-center">
+    <TomTomMap city='Aubagne' apikey='hBGB8rd9Z05DLKUCaLmXbLOvQIblJEJ2'/>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+import TomTomMap from 'components/TomTomMap.vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  components: { TomTomMap }
+})
+</script>
